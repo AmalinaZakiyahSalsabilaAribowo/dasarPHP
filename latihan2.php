08710599ad04943e99edad4c31dc1bd1bda98fe4
AmalinaZakiyahSalsabilaAribowo <42019807+AmalinaZakiyahSalsabilaAribowo@users.noreply.github.com>
2019-05-23 00:58:53 +0700

    modul 3

--- latihan2.php
+++ latihan2.php
@@ -1,14 +1,5 @@
-<?php
-  $x='4';
-  $y='5';
-
-  echo "<h3> Hasil Operasi Menggunakan Operator Perbandingan </h3>";
-  echo "x = $x <br>";
-  echo "y = $y <br>";
-  if ($x == $y) {
-    echo "x sama dengan y";
-  }
-  else if ($x != $y){
-    echo " x tidak sama dengan y";
-  }
- ?>
+<?php $nilai = 50; if ($nilai >= 75) {
+    echo "Nilai Anda $nilai, Anda LULUS";
+} else {
+    echo "Nilai Anda $nilai, Anda GAGAL"; }
+?>
--- latihan4.php
+++ latihan4.php
@@ -1,20 +1,7 @@
-<?php
-    $a=1;
-    echo $a++."<br/>";
-    echo $a."<br/>";
-    echo "<br>";
-
-    $b=1;
-    echo ++$b."<br/>";
-    echo $b."<br/>";
-    echo "<br>";
-
-    $c=2;
-    echo $c--."<br/>";
-    echo $c."<br/>";
-    echo "<br>";
-
-    $d = 2;
-    echo +--$d."<br/>";
-    echo $d."<br/>";
-?>
+<?php $user=""; if
+  (!isset($user)) {
+      echo "Variabel tidak ada/belum terbentuk";
+    } else {
+      echo "Variabel ada";
+    }
+ ?>
